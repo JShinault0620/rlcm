0bfb6f0c0db503623d6dd6c0067a515bc148461b
JShinault0620 <jshincool@gmail.com>
2025-02-10 20:24:09 -0500

    create contact modal

--- front/src/components/CreateModal.tsx
+++ front/src/components/CreateModal.tsx
@@ -1,22 +1,84 @@
-import {Modal, Button} from 'react-bootstrap'
+import React, { useState } from 'react'
+import {Modal, Button, Form} from 'react-bootstrap'
+import { useContactsContext } from '../context/contacts.tsx'
 
-const CreateModal = () => {
+interface ModalProps {
+    show: boolean
+    handleClose: () => void
+}
+
+const CreateModal: React.FC<ModalProps> = ({ show, handleClose }) => {
+    const { addContact } = useContactsContext()
+    const [ firstName, setFirstName ] = useState('')
+    const [ lastName, setLastName ] = useState('')
+    const [ phoneNumber, setPhoneNumber ] = useState('')
+    const [ email, setEmail ] = useState('')
+
+    const handleAddContact = () => {
+        if (!firstName || !lastName || !phoneNumber || !email) return
+
+        addContact({ firstName, lastName, phoneNumber, email })
+        setFirstName('')
+        setLastName('')
+        setPhoneNumber('')
+        setEmail('')
+
+        handleClose()
+    }
+
     return (
         <div className="modal show" style={{ display: 'block', position: 'initial' }}>
-            <Modal.Dialog>
+            <Modal show={show} onHide={handleClose} centered>
                 <Modal.Header closeButton>
                     <Modal.Title>Create Contact</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <p>Modal body text goes here.</p>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter first name"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter last name"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter phone number"
+                                value={phoneNumber}
+                                onChange={(e) => setPhoneNumber(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form>
                 </Modal.Body>
 
                 <Modal.Footer>
-                    <Button variant="secondary">Close</Button>
-                    <Button variant="primary">Save</Button>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="primary" onClick={handleAddContact}>Save</Button>
                 </Modal.Footer>
-            </Modal.Dialog>
+            </Modal>
         </div>
     )
 }
--- front/src/components/ViewContacts.tsx
+++ front/src/components/ViewContacts.tsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Container, Row, Col, Table, Button} from 'react-bootstrap'
 import {ContactsProvider, useContactsContext} from '../context/contacts.tsx'
+import CreateModal from './CreateModal'
 
 const ContactsTable: React.FC = () => {
   const { contacts, addContact, deleteContact } = useContactsContext()
+  const [ showModal, setShowModal ] = useState(false)
 
   return (
     <Container>
-      <ContactsProvider>
-        <Row className="justify-content-center">
-          <Col className="my-3 col-auto"><Button onClick={() => addContact({firstName: 'Test', lastName: 'TestLast', phoneNumber: '123123123', email: 'test@email.com12'})}>Create</Button></Col>
-          <Table className="text-center table-striped table-sm">
-            <thead>
-              <tr>
-                <th>FirstName</th>
-                <th>LastName</th>
-                <th>Phone</th>
-                <th>Email</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {contacts.map((contact) => {
-                return (
-                  <tr>
-                    <td>{contact.firstName}</td>
-                    <td>{contact.lastName}</td>
-                    <td>{contact.phoneNumber}</td>
-                    <td>{contact.email}</td>
-                    <td><Button className="btn-danger" onClick={() => {deleteContact(contact.id)}}>Delete</Button></td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </Table>
-        </Row>
-      </ContactsProvider>
+      <Row className="justify-content-center">
+        <Col className="my-3 col-auto"><Button onClick={() => setShowModal(true)}>Create</Button></Col>
+        <Table className="text-center table-striped table-sm">
+          <thead>
+            <tr>
+              <th>FirstName</th>
+              <th>LastName</th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => {
+              return (
+                <tr>
+                  <td>{contact.firstName}</td>
+                  <td>{contact.lastName}</td>
+                  <td>{contact.phoneNumber}</td>
+                  <td>{contact.email}</td>
+                  <td><Button className="btn-danger" onClick={() => {deleteContact(contact.id)}}>Delete</Button></td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      </Row>
+
+      <CreateModal show={showModal} handleClose={() => setShowModal(false)}/>
     </Container>
   )
 }
