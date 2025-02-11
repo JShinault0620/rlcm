074f12be6e50af0a2da2e2afd8ff4d03754e11a1
JShinault0620 <jshincool@gmail.com>
2025-02-10 22:31:41 -0500

    add h1, combine edit/delete <td>'s

--- front/src/components/ViewContacts.tsx
+++ front/src/components/ViewContacts.tsx
@@ -10,6 +10,7 @@ const ContactsTable: React.FC = () => {
 
   return (
     <Container>
+      <Row><h1 className="text-light text-center mt-3">Contact Manager</h1></Row>
       <Row className="justify-content-center">
         <Col className="my-3 col-auto"><Button onClick={() => {setEditContactID(-1); setShowModal(true)}}>Create</Button></Col>
         <Table className="text-center table-striped table-sm">
@@ -19,7 +20,6 @@ const ContactsTable: React.FC = () => {
               <th>LastName</th>
               <th>Phone</th>
               <th>Email</th>
-              <th></th>
               <th></th>
             </tr>
           </thead>
@@ -31,8 +31,10 @@ const ContactsTable: React.FC = () => {
                   <td>{contact.lastName}</td>
                   <td>{contact.phoneNumber}</td>
                   <td>{contact.email}</td>
-                  <td><Button className="btn-warning" onClick={() => {setEditContactID(contact.id); setShowModal(true)}}>Edit</Button></td>
-                  <td><Button className="btn-danger" onClick={() => {deleteContact(contact.id)}}>Delete</Button></td>
+                  <td>
+                    <Button className="btn-warning" onClick={() => {setEditContactID(contact.id); setShowModal(true)}}>Edit</Button>
+                    <Button className="btn-danger" onClick={() => {deleteContact(contact.id)}}>Delete</Button>
+                  </td>
                 </tr>
               )
             })}
