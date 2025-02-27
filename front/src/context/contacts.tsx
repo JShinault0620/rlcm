50e3c7b5763a00210d66f489cd7de4f19d47296b
JShinault0620 <jshincool@gmail.com>
2025-02-27 18:29:29 -0500

    fetch contact list

--- front/src/context/contacts.tsx
+++ front/src/context/contacts.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, createContext, useContext, useState } from 'react'
+import React, { ReactNode, createContext, useContext, useState, useEffect } from 'react'
 
 interface Contact {
     id?: any
@@ -19,22 +19,35 @@ interface ContactContextType {
 const ContactsContext = createContext<ContactContextType | undefined>(undefined)
 
 export const ContactsProvider : React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [contacts, setContacts] = useState<Contact[]>([
-        {
-            id: 0,
-            firstName: 'Jonny',
-            lastName: 'Shin',
-            phoneNumber: '123-456-7890',
-            email: 'test@email.com'
-        },
-        {
-            id: 1,
-            firstName: 'Random',
-            lastName: 'Person',
-            phoneNumber: '349-324-1421',
-            email: 'random@gmail.com'
-        }
-    ])
+    const [contacts, setContacts] = useState<Contact[]>([])
+
+    const refreshContacts = () => {
+        fetch('/api/contacts.cfc?method=getcontacts')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('There was a problem fetching the contact list.')
+                }
+                return res.json()
+            })
+            .then((data) => {
+                setContacts(JSON.parse(data).map((c: any ) => {
+                    return {
+                        id: c.ID,
+                        firstName: c.FIRSTNAME,
+                        lastName: c.LASTNAME,
+                        phoneNumber: c.PHONENUMBER,
+                        email: c.EMAIL
+                    }
+                }))
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
+
+    useEffect(() => {
+        refreshContacts()
+    },[])
 
     const addContact = (contact: Contact) => {
         contact.id = contacts.length === 0 ? 0 : Math.max(...contacts.map(c => c.id)) + 1
